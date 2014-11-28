9b1efc58473832c543676aeef9cb6f8fb95bc3f3
wlh <wlhmyit@126.com>
2014-11-28 22:52:28 +0800

    add ajax

--- QMap.js
+++ QMap.js
@@ -1,25 +1,47 @@
 
-/*
-+ 全国地级市的多边形数据
-+ 
-+
-+
-*/
-
 (function(global){
-
-	var ajax = function(path, callback){
-		var http = new XMLHttpRequest();
-		http.open('GET', path, true);
-		http.send(null);
-		http.onreadystatechange = function(){
-			if(http.readyState === 4){
-				
-			}else{
-
+	/*
+	 + @class obj: 处理ajax请求
+	 + @method: getJSON从服务器拉取数据
+	 + @params： uri请求资源的路径;callback获取服务器数据的回调函数
+	 */
+	var ajax = {
+		getJSON: function(uri, callback){
+			var httpRequest = null;
+			if(window.XMLHttpRequest){
+				httpRequest = new XMLHttpRequest();	
 			}
-		};
-	}
-
+			if(window.ActiveXObject){
+				httpRequest = new ActiveXObject('"Microsoft.XMLHTTP"');
+			}
+			httpRequest.onreadystatechange = function(){
+				if(httpRequest.readyState === 4){
+					if(httpRequest.status === 200){
+						var str = httpRequest.responseText;
+						if(JSON && JSON.parse){
+							return callback(JSON.parse(str));
+						}else{
+							return callback( (new Function("return " + str))() );	
+						}
+					}
+					return callback(null);
+				}
+				return callback(null);
+			};
+			httpRequest.open('GET', uri, true);
+			httpRequest.send();
+		}
+	};
+	/*
+	 + @class obj: 构建矢量图形的类
+	 + 
+	 + 
+	 */
+	var vmap = {
+		ajax: ajax,
+		polygon: function(){
+			
+		}
+	};
 
 })(window);
